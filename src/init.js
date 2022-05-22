678e95970b49baf67061ec431d8bd17c993377bd
codingfun02 <codingfun@naver.com>
2022-05-22 16:28:50 +0900

    #0 Babel & Webpack

---
+++ src/init.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import express from "express";
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`✅ Server Listening at port ${PORT}`));
+
+export default app;
